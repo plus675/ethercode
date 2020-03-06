fc0c11eae8c1bb4a81395ed0390273f7db29ebec
plus675 <32140757+plus675@users.noreply.github.com>
2020-03-06 16:45:23 +0530

    Update NewTokenVraj.sol

--- NewTokenVraj.sol
+++ NewTokenVraj.sol
@@ -6,7 +6,7 @@ import "./ERC20Standard.sol";
 contract NewToken is ERC20Standard {
 	constructor() public {
 		totalSupply = 1000000;
-		name = "Vraj Token Coin";
+		name = "VrajTokenCoin";
 		decimals = 4;
 		symbol = "VRAJTC";
 		version = "1.0";
